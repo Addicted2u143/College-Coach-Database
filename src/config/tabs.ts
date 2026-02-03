52dd19a20e213d443507eb4d80f001984214716f
Addicted2u143 <nicole.amperse@gmail.com>
2026-02-03 15:23:00 -0700

    Refactor TABS to use TabDef type and add groups

--- src/config/tabs.ts
+++ src/config/tabs.ts
@@ -1,80 +1,93 @@
-import type { TabKey } from "../lib/types";
+// src/config/tabs.ts
+import type { TabDef, TabKey } from "../lib/types";
 
-export const TABS: Array<{ key: TabKey; label: string; csvUrl: string }> = [
+export const TABS: TabDef[] = [
   {
     key: "FBS",
     label: "FBS",
+    group: "Football",
     csvUrl:
-      "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC7JeyZJaNuU-jdhjDGgzOvj4-ZLxORZ008FnhdsFgvK5KV93tT1lYfkvCDw4h5g/pub?output=csv"
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC7JeyZJaNuU-jdhjDGgzOvj4-ZLxORZ008FnhdsFgvK5KV93tT1lYfkvCDw4h5g/pub?output=csv",
   },
   {
     key: "FCS",
     label: "FCS",
+    group: "Football",
     csvUrl:
-      "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC7JeyZJaNuU-jdhjDGgzOvj4-ZLxORZ008FnhdsFgvK5KV93tT1lYfkvCDw4h5g/pub?gid=1355347771&single=true&output=csv"
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC7JeyZJaNuU-jdhjDGgzOvj4-ZLxORZ008FnhdsFgvK5KV93tT1lYfkvCDw4h5g/pub?gid=1355347771&single=true&output=csv",
   },
   {
     key: "DII",
     label: "DII",
+    group: "Football",
     csvUrl:
-      "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC7JeyZJaNuU-jdhjDGgzOvj4-ZLxORZ008FnhdsFgvK5KV93tT1lYfkvCDw4h5g/pub?gid=56535029&single=true&output=csv"
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC7JeyZJaNuU-jdhjDGgzOvj4-ZLxORZ008FnhdsFgvK5KV93tT1lYfkvCDw4h5g/pub?gid=56535029&single=true&output=csv",
   },
   {
     key: "DIII",
     label: "DIII",
+    group: "Football",
     csvUrl:
-      "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC7JeyZJaNuU-jdhjDGgzOvj4-ZLxORZ008FnhdsFgvK5KV93tT1lYfkvCDw4h5g/pub?gid=1086413570&single=true&output=csv"
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC7JeyZJaNuU-jdhjDGgzOvj4-ZLxORZ008FnhdsFgvK5KV93tT1lYfkvCDw4h5g/pub?gid=1086413570&single=true&output=csv",
   },
   {
     key: "NAIA",
     label: "NAIA",
+    group: "Football",
     csvUrl:
-      "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC7JeyZJaNuU-jdhjDGgzOvj4-ZLxORZ008FnhdsFgvK5KV93tT1lYfkvCDw4h5g/pub?gid=1429911104&single=true&output=csv"
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC7JeyZJaNuU-jdhjDGgzOvj4-ZLxORZ008FnhdsFgvK5KV93tT1lYfkvCDw4h5g/pub?gid=1429911104&single=true&output=csv",
   },
   {
     key: "Sprint",
     label: "Sprint",
+    group: "Football",
     csvUrl:
-      "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC7JeyZJaNuU-jdhjDGgzOvj4-ZLxORZ008FnhdsFgvK5KV93tT1lYfkvCDw4h5g/pub?gid=2045840484&single=true&output=csv"
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC7JeyZJaNuU-jdhjDGgzOvj4-ZLxORZ008FnhdsFgvK5KV93tT1lYfkvCDw4h5g/pub?gid=2045840484&single=true&output=csv",
   },
   {
     key: "JuCo",
     label: "JuCo",
+    group: "Football",
     csvUrl:
-      "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC7JeyZJaNuU-jdhjDGgzOvj4-ZLxORZ008FnhdsFgvK5KV93tT1lYfkvCDw4h5g/pub?gid=1057782639&single=true&output=csv"
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC7JeyZJaNuU-jdhjDGgzOvj4-ZLxORZ008FnhdsFgvK5KV93tT1lYfkvCDw4h5g/pub?gid=1057782639&single=true&output=csv",
   },
   {
     key: "Post-Graduate",
     label: "Post-Graduate",
+    group: "Football",
     csvUrl:
-      "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC7JeyZJaNuU-jdhjDGgzOvj4-ZLxORZ008FnhdsFgvK5KV93tT1lYfkvCDw4h5g/pub?gid=2097889449&single=true&output=csv"
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC7JeyZJaNuU-jdhjDGgzOvj4-ZLxORZ008FnhdsFgvK5KV93tT1lYfkvCDw4h5g/pub?gid=2097889449&single=true&output=csv",
   },
   {
     key: "Canada",
     label: "Canada",
+    group: "Football",
     csvUrl:
-      "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC7JeyZJaNuU-jdhjDGgzOvj4-ZLxORZ008FnhdsFgvK5KV93tT1lYfkvCDw4h5g/pub?gid=814992836&single=true&output=csv"
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC7JeyZJaNuU-jdhjDGgzOvj4-ZLxORZ008FnhdsFgvK5KV93tT1lYfkvCDw4h5g/pub?gid=814992836&single=true&output=csv",
   },
   {
     key: "Womens Flag",
     label: "Womens Flag",
+    group: "Football",
     csvUrl:
-      "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC7JeyZJaNuU-jdhjDGgzOvj4-ZLxORZ008FnhdsFgvK5KV93tT1lYfkvCDw4h5g/pub?gid=1723697523&single=true&output=csv"
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC7JeyZJaNuU-jdhjDGgzOvj4-ZLxORZ008FnhdsFgvK5KV93tT1lYfkvCDw4h5g/pub?gid=1723697523&single=true&output=csv",
   },
 
-  // Other sports (tabs exist now; add data later)
+  // ---- Other Sports (NEW) ----
   {
     key: "Mens Basketball",
-    label: "Mens Basketball",
+    label: "Men’s Basketball",
+    group: "Other Sports",
     csvUrl:
-      "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC7JeyZJaNuU-jdhjDGgzOvj4-ZLxORZ008FnhdsFgvK5KV93tT1lYfkvCDw4h5g/pub?gid=1547947146&single=true&output=csv"
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC7JeyZJaNuU-jdhjDGgzOvj4-ZLxORZ008FnhdsFgvK5KV93tT1lYfkvCDw4h5g/pub?gid=1547947146&single=true&output=csv",
   },
   {
     key: "Mens Baseball",
-    label: "Mens Baseball",
+    label: "Men’s Baseball",
+    group: "Other Sports",
     csvUrl:
-      "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC7JeyZJaNuU-jdhjDGgzOvj4-ZLxORZ008FnhdsFgvK5KV93tT1lYfkvCDw4h5g/pub?gid=1455310286&single=true&output=csv"
-  }
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC7JeyZJaNuU-jdhjDGgzOvj4-ZLxORZ008FnhdsFgvK5KV93tT1lYfkvCDw4h5g/pub?gid=1455310286&single=true&output=csv",
+  },
 ];
 
 export const DEFAULT_TAB: TabKey = "FBS";
